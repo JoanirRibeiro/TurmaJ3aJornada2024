114442513a3462a50ab035eddaff61d959cd188c
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2024-11-07 22:37:50 +0000

    criando elementos html com funções JavaScript

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criarCartao (
+    'Filosofia',
+    'O que é filosofia?',
+    'Traduzindo a palavra, filosofia significa "Amor ou desejo pela Sabedoria'
+)
+
+criarCartao (
+    'Filosofia',
+    'Quando surgiu a filosofia?',
+    'Surgiu na Grécia Antiga, aproximadamente no ano de 600 a.C.'
+)
+
+criarCartao (
+    'História',
+    'Em que ano aconteceu a Revolução Francesa?',
+    'A Revolução Francesa ocorreu em 1789'
+)
+
+criarCartao (
+    'Química',
+    'O que é um átomo?',
+    'A palavra significa "não divisível'
+)
